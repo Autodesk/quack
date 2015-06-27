7c2e9a5f5cf63d4a7cebb6a6b907c914016748ce
Love Sharma <contact@lovesharma.com>
2015-06-28 01:34:14 +0800

    bug fixes

--- quack.py
+++ quack.py
@@ -34,6 +34,12 @@ except ImportError as import_error:
     subprocess.call('pip install pyyaml'.split())
     import yaml
 
+try:
+    import git
+except ImportError as import_error:
+    subprocess.call('pip install gitpython'.split())
+    import git
+
 
 def _get_config():
     """Return yaml configuration."""
@@ -51,27 +57,51 @@ def _fetch_modules():
     _create_dir(_modules)
     with open('.gitignore', 'r') as file_pointer:
         ignore_list = list(set(file_pointer.read().split('\n')))
+    repo = git.Repo('.')
     for module in _CONFIG.get('modules').items():
         _remove_dir(module[0])
-        command = 'git submodule add --force %s %s/%s' % (
-            module[1]['repository'], _modules, module[0])
-        print '--> ' + command
-        os.popen(command)
+        # command = 'git submodule add --force %s %s/%s' % (
+        #     module[1]['repository'], _modules, module[0])
+        sub_module = repo.create_submodule(
+            'pyan', _modules + '/' + module[0],
+            url=module[1]['repository'],
+            branch=module[1].get('branch', 'master')
+        )
+        # if not os.path.exists(_modules + '/' + module[0]):
+        #     print '--> ' + command
+        #     os.popen(command)
         # print command, module
+        print sub_module, sub_module.hexsha, sub_module.binsha, dir(sub_module)
         path = module[1].get('path', '')
         from_path = '%s/%s/%s' % (_modules, module[0], path)
         # print path, from_path
-        if (path and os.path.exists(from_path)) or not path:
+        is_exists = os.path.exists(from_path)
+        if (path and is_exists) or not path:
             shutil.copytree(from_path, module[0])
-            os.popen('git submodule deinit -f %s/%s' % (_modules, module[0]))
-            os.popen('git rm --cached %s/%s' % (_modules, module[0]))
+        elif not is_exists:
+            print '%s folder does not exists. Skipped.' % path
+
+        # Remove submodule.
+        os.popen('git submodule deinit -f %s/%s' % (_modules, module[0]))
+        os.popen('git rm --cached %s/%s' % (_modules, module[0]))
+        if os.path.isfile('.gitmodules'):
             os.popen('git rm --cached .gitmodules')
             os.popen('rm .gitmodules')
-            with open('.gitignore', 'a') as file_pointer:
-                if module[0] not in ignore_list:
-                    file_pointer.write('\n' + module[0])
-                    ignore_list.append(module[0])
+
+        with open('.gitignore', 'a') as file_pointer:
+            if module[0] not in ignore_list:
+                file_pointer.write('\n' + module[0])
+                ignore_list.append(module[0])
 
 _CONFIG = _get_config()
+# print dir(git)
+# repo = git.Repo('.')
+# print repo, dir(repo)
+# sm = repo.create_submodule(
+#     'pyan', '.quack/modules/pyan',
+#     url='https://github.com/kra3/py-ga-mob.git', branch='master')
+# sm.binsha = '998df5359ef1faada2f530c8840b82d7342a100e'
+# sm.update()
+# print sm, dir(sm)
 if _CONFIG:
     _fetch_modules()

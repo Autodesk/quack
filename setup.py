93bf2ab07d20161ccdcc0e5efaf14e3e20b696e2
Love Sharma <contact@lovesharma.com>
2015-06-28 03:13:51 +0800

    setup script added

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Quack setup script."""
+
+from setuptools import setup
+
+setup(
+    name='quack',
+    version='0.0.1',
+    packages=['quack'],
+
+    # dependencies
+    install_requires=[
+        'gitpython',
+        'pyyaml'
+    ],
+
+    # metadata for upload to PyPI
+    author='Love Sharma',
+    author_email='contact@lovesharma.com',
+    description=('Insert specific module / folder from given'
+                 'repositories in yaml configurations.'),
+    license='MIT',
+    keywords='quack build submodule module dependencies',
+    url='https://github.com/zonito/quack',  # project homepage
+    download_url='https://github.com/zonito/quack/archive/0.0.1.tar.gz',
+
+    entry_points={
+        'console_scripts': [
+            'quack=quack.quack:main'
+        ]
+    }
+)

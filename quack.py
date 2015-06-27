93420d25d1585b9235d02cf72ba10553323427f3
Love Sharma <contact@lovesharma.com>
2015-06-27 10:50:46 +0800

    quack update

--- quack.py
+++ quack.py
@@ -4,24 +4,26 @@ import os
 import subprocess
 
 
-def _create_dir(directory):
+def _create_dir(directory, is_fresh=False):
     """Create directory."""
+    if os.path.exists(directory) and is_fresh:
+        shutil.rmtree(directory)
     if not os.path.exists(directory):
         os.makedirs(directory)
 _create_dir('.quack')
 
 try:
     import yaml
-except ImportError as ie:
-    is_pip_installed = '<command>' in os.popen('pip').read()
-    if not is_pip_installed:
+except ImportError as import_error:
+    IS_PIP_INSTALLED = '<command>' in os.popen('pip').read()
+    if not IS_PIP_INSTALLED:
         from urllib2 import urlopen
-        url = ('https://raw.githubusercontent.com/pypa/'
-               'pip/master/contrib/get-pip.py')
-        fp = urlopen(url)
+        _URL = ('https://raw.githubusercontent.com/pypa/'
+                'pip/master/contrib/get-pip.py')
+        _FP = urlopen(_URL)
         # Open our local file for writing
-        with open('.quack/' + os.path.basename(url), "wb") as local_file:
-            local_file.write(fp.read())
+        with open('.quack/' + os.path.basename(_URL), "wb") as local_file:
+            local_file.write(_FP.read())
         subprocess.call('python .quack/get-pip.py')
         subprocess.call('rm .quack/get-pip.py')
     subprocess.call('pip install pyyaml'.split())
@@ -31,24 +33,25 @@ except ImportError as ie:
 def _get_config():
     """Return yaml configuration."""
     # Make it executable from : http://stackoverflow.com/questions/15587877/
-    with open('quack.yaml') as fp:
-        return yaml.load(fp)
+    with open('quack.yaml') as file_pointer:
+        return yaml.load(file_pointer)
     return {}
 
-configs = _get_config()
-if configs:
-    _modules = '.quack/modules'
-    _create_dir(_modules)
-    for module in configs.get('modules').items():
-        subprocess.call('rm -rf %s' % module[0])
-        command = 'git submodule add -f %s %s/%s' % (
-            module[1]['repository'], _modules, module[0])
-        subprocess.call(
-            command, stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
+_CONFIG = _get_config()
+if _CONFIG:
+    _MODULES = '.quack/modules'
+    _create_dir(_MODULES)
+    for module in _CONFIG.get('modules').items():
+        import shutil
+        if os.path.exists(module[0]):
+            shutil.rmtree(module[0])
+        command = 'git submodule add --force %s %s/%s' % (
+            module[1]['repository'], _MODULES, module[0])
+        print command
+        os.popen(command)
         print command, module
         path = module[1].get('path', '')
-        from_path = '%s/%s/%s' % (_modules, module[0], path)
+        from_path = '%s/%s/%s' % (_MODULES, module[0], path)
         print path, from_path
-        import shutil
         if (path and os.path.exists(from_path)) or not path:
             shutil.copytree(from_path, module[0])
